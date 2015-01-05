311187d43c5657a608d5492eaada1a7fe4e62abc
Arnout Kazemier <info@3rd-Eden.com>
2015-01-05 15:04:29 +0100

    [refactor] inline the zipline so we can use custom names and multiple intances

--- index.js
+++ index.js
@@ -1,31 +1,115 @@
 'use strict';
 
-//
-// The major release version of this plugin which is used as value for the
-// cookie check.
-//
-var major = require('./package.json').version.split('.').shift()
-  , line = require('fs').readFileSync(__dirname +'/line.gz');
+var destroy = require('demolish')
+  , path = require('path')
+  , zlib = require('zlib')
+  , fs = require('fs');
+
+var obfheader = /^(Accept-EncodXng|X-cept-Encoding|X{15}|~{15}|-{15})$/i
+  , obfvalue = /^((gzip|deflate)\s*,?\s*)+|[X\~\-]{4,13}$/i;
 
 /**
- * Check if the given headers support gzip or deflate for compression.
+ * Zipline: An GZIP detection utility.
  *
- * @param {Object} headers Headers of the incoming request.
- * @returns {String|Undefined}
+ * @constructor
+ * @param {Object} options Optional configuration of your zipline.
+ * @api public
+ */
+function Zipline(options) {
+  if (!this) return new Zipline(options);
+  options = options || {};
+
+  this.pathname = options.pathname || '/zipline.js';
+  this.name = options.name || 'zipline';
+  this.buffer = null;
+
+  this.initialize();
+}
+
+//
+// Inherit from EventEmitter3 so we can queue middleware requests while we're
+// still compiling the line.
+//
+Zipline.prototype.__proto__ = require('eventemitter3').prototype;
+
+/**
+ * Initialize the GZIP buffer.
+ *
  * @api private
  */
-function zipline(headers) {
+Zipline.prototype.initialize = function initialize(options) {
+  var line = fs.readFileSync(path.join(__dirname, 'line.js'), 'utf-8')
+  .replace('{zipline:major}', Zipline.major)
+  .replace('{zipline:minor}', Zipline.minor)
+  .replace('{zipline:patch}', Zipline.patch)
+  .replace('{zipline:name}', this.name)
+  .replace(/[\n|\r]+/g, '')
+  .replace(/\s{2,}/, '');
+
+  zlib.gzip(new Buffer(line), function gzipped(err, buffer) {
+    if (err) return this.emit('error', err);
+
+    this.buffer = buffer;
+    this.emit('initialize');
+  }.bind(this));
+};
+
+/**
+ * Return a middleware layer which parse the accept headers.
+ *
+ * @returns {Function}
+ * @api public
+ */
+Zipline.prototype.middleware = function middleware() {
+  var zipline = this;
+
+  return function layer(req, res, next) {
+    req[zipline.name] = Zipline.accepts(req, zipline.name);
+
+    if (zipline.pathname !== req.url) return next();
+    if (!zipline.buffer) return zipline.once('initialize', layer.bind(layer, req, res, next));
+
+    res.statusCode = 200;
+    res.setHeader('Content-Encoding', 'gzip');
+    res.setHeader('Content-Type', 'text/javascript');
+    res.setHeader('Content-Length', zipline.buffer.length);
+    res.end(zipline.buffer);
+  };
+};
+
+/**
+ * Completely destroy and clean up the Zipline instance.
+ *
+ * @type {Function}
+ * @returns {Boolean}
+ * @api public
+ */
+Zipline.prototype.destroy = destroy('name, buffer, pathname', {
+  after: 'removeAllListeners'
+});
+
+/**
+ * Parse the Accept-Encoding headers and figure if the user supports any form of
+ * deflation.
+ *
+ * @param {Request} req Incoming HTTP request.
+ * @param {String} name Name of the zipline cookie we need to search for.
+ * @returns {String|Undefined}
+ * @api public
+ */
+Zipline.accepts = function accepts(req, name) {
+  var i, value
+    , raw = req.rawHeaders || []
+    , headers = req.headers || req
+    , ua = (headers['user-agent'] || '')
+    , line = (name || 'zipline') +'='+ Zipline.major;
+
   //
-  // Check for the bypass cookie which can be set
+  // Check for the bypass cookie which can be set.
   //
-  if (headers.cookie && ~headers.cookie.indexOf('zipline='+ major)) {
+  if (headers.cookie && ~headers.cookie.indexOf(line)) {
     return 'gzip';
   }
-
-  var obfheader = /^(Accept-EncodXng|X-cept-Encoding|X{15}|~{15}|-{15})$/i
-    , obfvalue = /^((gzip|deflate)\s*,?\s*)+|[X\~\-]{4,13}$/i
-    , ua = (headers['user-agent'] || '')
-    , key, value;
 
   //
   // Detect broken gzip encoding on Internet Explorer 5 & 6.
@@ -49,39 +133,41 @@ function zipline(headers) {
   //
   // @see developer.yahoo.com/blogs/ydn/posts/2010/12/pushing-beyond-gzipping
   //
-  for (key in headers) {
-    if (obfheader.test(key) && (value = obfvalue.exec(headers[key]))) {
+  if (!raw.length) for (i in headers) {
+    if (obfheader.test(i) && (value = obfvalue.exec(headers[i]))) {
       return value[1];
     }
+  } else for (i = 0; i < raw.length; i++) {
+    if (obfheader.test(raw[i]) && (value = obfvalue.exec(raw[i + 1]))) {
+      return value[1];
+    }
+
+    //
+    // We need to increment the iteration as the raw headers are stored as
+    // [key, value, key, value] and we're only interested in testing the key's
+    // not the values.
+    //
+    i++;
   }
 
   return undefined;
-}
+};
 
 /**
- * Respond to /zipline.js requests in addition to automatically adding
- * a `zipline` property to the incoming HTTP request.
+ * Parsed version number of the module which is needed for tagging the things.
  *
- * @param {Request} req Incoming HTTP request.
- * @param {Response} res Outgoing HTTP response.
- * @param {Function} next Continuation callback.
- * @api public
+ * @type {String}
+ * @public
  */
-function middleware(req, res, next) {
-  req.zipline = zipline(req.headers);
+Zipline.major = '0';
+Zipline.minor = '0';
+Zipline.patch = '0';
 
-  if (req.url !== '/zipline.js') return next();
-
-  res.statusCode = 200;
-  res.setHeader('Content-Encoding', 'gzip');
-  res.setHeader('Content-Length', line.length);
-  res.setHeader('Content-Type', 'text/javascript');
-
-  res.end(line);
-}
+require('./package.json').version.split('.').forEach(function each(version, i) {
+  Zipline[this[i]] = version;
+}, ['major', 'minor', 'patch']);
 
 //
-// Expose the module. Expose all the things.
+// Expose the Zipline interface.
 //
-zipline.middleware = middleware;
-module.exports = zipline;
+module.exports = Zipline;
