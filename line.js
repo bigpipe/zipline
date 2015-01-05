d542ba9858bc63581f5997c770b365f40e7e8479
Arnout Kazemier <info@3rd-Eden.com>
2015-01-05 11:52:45 +0100

    [major] Also store zipline in session & localStorage by default

--- line.js
+++ line.js
@@ -1,13 +1,10 @@
-'use strict';
+(function(z,m){
+  try{document.cookie=z+'='+m+'; expires=Thu, 18 Dec 2913 12:00:00 UTC';}
+  catch(e){}
 
-var fs = require('fs')
-  , zlib = require('zlib')
-  , major = require('./package.json').version.split('.').shift()
-  , data = 'document.cookie="zipline='+ major +'; expires=Thu, 18 Dec 2913 12:00:00 UTC";';
+  try{sessionStorage.setItem(z,m);}
+  catch(e){}
 
-zlib.gzip(new Buffer(data), function gzip(err, buffer) {
-  if (err) process.exit(1);
-
-  fs.writeFileSync(__dirname +'/line.gz', buffer);
-  process.exit(0);
-});
+  try{localStroage.setItem(z,m);}
+  catch(e){}
+}('{zipline:key}','{zipline:major}'));
